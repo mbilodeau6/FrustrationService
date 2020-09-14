d6bf1285c98fb76cdc509e0fd263893f4aa8c341
Michael Bilodeau <mbilodeau6@hotmail.com>
2020-09-13 19:24:59 -0700

    Take gameId as parameter for Get game

--- src/FrustrationService/CardREST.java
+++ src/FrustrationService/CardREST.java
@@ -1,22 +1,39 @@
 package FrustrationService;
 
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
-@Path("/cards")
+@Path("/games")
 public class CardREST {
+	private static Map<String, GameState> games;
+	
+	// TODO: Replace with singleton that has its own rest service to manage 
+	// game lifecycle.
+	static {
+		CardREST.games = new HashMap<>();
+		
+		List<String> names = Arrays.asList("Michael", "Lisa", "Terry", "Julien");
+		CardREST.games.put("A",  new GameState(names));
+		
+		names = Arrays.asList("Michael", "Lisa");
+		CardREST.games.put("B", new GameState(names));
+	}
+	
 	@GET
+	@Path("{gameId}")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getTest() {
-//		List<String> names = Arrays.asList("Michael", "Lisa", "Terry", "Julien");
-		List<String> names = Arrays.asList("Michael", "Lisa");
-		GameState gameState = new GameState(names);
+	public Response getGame(@PathParam("gameId") String gameId) {
+		// TODO: Add error handling if gameId doesn't exist
+		GameState gameState = games.get(gameId);
 		
 		return Response.ok().entity(gameState.getAsDTO()).build();
 	}
