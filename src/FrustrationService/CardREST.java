a74ef7e5cf9b0901dc6a7af7a57a5a05a43e11cb
Michael Bilodeau <mbilodeau6@hotmail.com>
2020-09-13 19:01:04 -0700

    Set up for 2 players

--- src/FrustrationService/CardREST.java
+++ src/FrustrationService/CardREST.java
@@ -14,7 +14,8 @@ public class CardREST {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getTest() {
-		List<String> names = Arrays.asList("Michael", "Lisa", "Terry", "Julien");
+//		List<String> names = Arrays.asList("Michael", "Lisa", "Terry", "Julien");
+		List<String> names = Arrays.asList("Michael", "Lisa");
 		GameState gameState = new GameState(names);
 		
 		return Response.ok().entity(gameState.getAsDTO()).build();
